304195208c1cd2f4dead90050ccb7b983b17b5bc
Logan Wilkerson <lcwilker@gmail.com>
2026-01-14 15:02:57 -0500

    more starfield shader work. still not super happy with glitter. gonna look for some other background options

--- game/world/debug/world_debug.gd
+++ game/world/debug/world_debug.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
-@onready var texture_rect: TextureRect = $Stage/TextureRect
+@onready var starfield_1: TextureRect = $Stage/Starfield1
 @export var scroll_velocity := Vector2(.001, 0)
 var scroll := Vector2.ZERO
 func _physics_process(delta: float) -> void:
 	scroll += scroll_velocity * delta
-	texture_rect.material.set("shader_parameter/offset", scroll)
+	starfield_1.material.set("shader_parameter/offset", scroll)
