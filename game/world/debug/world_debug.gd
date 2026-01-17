87fafdd8360f3e4b7b8a33454031a294a6052f83
Logan Wilkerson <lcwilker@gmail.com>
2026-01-17 01:04:32 -0500

    more working on mixing noise shader. working on nebula vibes.

--- game/world/debug/world_debug.gd
+++ game/world/debug/world_debug.gd
@@ -2,11 +2,8 @@
 extends Node2D
 
 @onready var starfield_1: TextureRect = $Stage/Starfield1
-@onready var noise_mixer: TextureRect = $Stage/Nebula/NoiseMixer
 @export var scroll_velocity := Vector2(.001, 0)
 var scroll := Vector2.ZERO
-@export var mix_ratio : float = 1.0
 func _physics_process(delta: float) -> void:
 	scroll += scroll_velocity * delta
 	starfield_1.material.set("shader_parameter/offset", scroll)
-	noise_mixer.material.set("shader_parameter/mix_ratio", mix_ratio)
