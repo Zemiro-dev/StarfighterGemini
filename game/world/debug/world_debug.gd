feaf0b4034014fb0e3e786656db32c2355737a52
Logan Wilkerson <lcwilker@gmail.com>
2026-01-27 22:59:17 -0500

    building out new asteroid beast claw look and feel

--- game/world/debug/world_debug.gd
+++ game/world/debug/world_debug.gd
@@ -1,8 +1,1 @@
 extends Node2D
-
-@onready var starfield_1: TextureRect = $Stage/Starfield1
-@export var scroll_velocity := Vector2(.001, 0)
-var scroll := Vector2.ZERO
-func _physics_process(delta: float) -> void:
-	scroll += scroll_velocity * delta
-	starfield_1.material.set("shader_parameter/offset", scroll)
