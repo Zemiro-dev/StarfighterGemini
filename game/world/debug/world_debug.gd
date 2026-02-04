084fa93874802cafcfd10009544199323c9c3e63
Logan Wilkerson <lcwilker@gmail.com>
2026-02-03 21:34:10 -0500

    adding laser fire demo animation

--- game/world/debug/world_debug.gd
+++ game/world/debug/world_debug.gd
@@ -1,1 +1,8 @@
 extends Node2D
+
+@onready var player_animation_player: AnimationPlayer = $Stage/Actors/Player/PlayerAnimationPlayer
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed('demo'):
+		player_animation_player.stop()
+		player_animation_player.play('fire');
