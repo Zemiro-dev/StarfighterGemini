20301a0b5698dd12b7558bcb4a192abe31d13eb5
Logan Wilkerson <lcwilker@gmail.com>
2026-03-22 14:40:35 -0400

    improvements to the collisions for the player

--- game/actors/physics_body_move_resolver.gd
+++ game/actors/physics_body_move_resolver.gd
@@ -1,42 +1,60 @@
 extends Node
 class_name PhysicsBodyMoveResolver
 
+const MAX_COLLISIONS = 5
+
 class MoveRequest extends Resource:
 	var body: PhysicsBody2D
 	var velocity: Vector2 = Vector2.ZERO
 	var steering: Steering
 	var should_slow: bool = false
+	
+func resolve_collisions(delta: float, velocity: Vector2, body: PhysicsBody2D) -> Vector2:
+	var motion := delta * velocity
+	var next_velocity := velocity
+	var has_been_attacked := false
+	for i in range(MAX_COLLISIONS):
+		var collision: KinematicCollision2D = body.move_and_collide(motion)
+		if collision:
+			var collider: Object = collision.get_collider()
+			var depth := collision.get_depth()
+			var remainder := collision.get_remainder()
+			var collider_type = GameActor.get_actor_type(collider)
+			var normal = collision.get_normal()
+			
+			## No matter what resolve overlap
+			if !is_zero_approx(depth):
+				body.global_position += depth * normal
+			
+			# Base Movement Effects
+			match (collider_type):
+				GameActor.ActorType.UNKNOWN, GameActor.ActorType.TERRAIN, GameActor.ActorType.ENEMY, GameActor.ActorType.DESTRUCTIBLE:
+					motion = remainder.bounce(normal)
+					next_velocity = next_velocity.bounce(normal)
+			### Damage and Additional Knockback 
+			match (collider_type):
+				GameActor.ActorType.ENEMY:
+					if !has_been_attacked and GameActor.check_and_attack(collider, body) >= 0:
+						next_velocity += next_velocity.normalized() * 2000.
+						print(next_velocity)
+						has_been_attacked = true
+		else:
+			break;
+	return next_velocity
 
 
 func resolve(request: MoveRequest, delta: float) -> Vector2:
 	var velocity := request.velocity
 	var steering := request.steering
-	var body :=	 request.body
 	if steering:
 		if !request.should_slow:
 			velocity = steering.steer(velocity, delta)
 		else:
 			velocity = steering.slow(velocity, delta)
 	
-	var collision: KinematicCollision2D = body.move_and_collide(velocity * delta)
-	if collision:
-			var collider: Object = collision.get_collider()
-			var depth := collision.get_depth()
-			var remainder := collision.get_remainder()
-			var collider_type = GameActor.get_actor_type(collider)
-			var normal = collision.get_normal()
-			## Base Movement Effects
-			match (collider_type):
-				GameActor.ActorType.UNKNOWN, GameActor.ActorType.TERRAIN, GameActor.ActorType.ENEMY, GameActor.ActorType.DESTRUCTIBLE:
-					var nvdot := normal.dot(velocity.normalized())
-					if nvdot <= 0.:
-						velocity = velocity.bounce(normal)
-			
-			## Damage and Additional Knockback 
-			match (collider_type):
-				GameActor.ActorType.ENEMY:
-					if GameActor.check_and_attack(collider, request.body) >= 0:
-						velocity += velocity.normalized() * 600.
+	velocity = resolve_collisions(delta, velocity, request.body)
 	if steering and steering.should_overspeed_break(velocity):
 		velocity = steering.overspeed_break(velocity, delta)
+	#print('next velocity ', velocity)
+	#print('')
 	return velocity
